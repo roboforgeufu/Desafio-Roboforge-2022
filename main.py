10c8d901bba7f42ebd5de3ce42f29a0b4ec5ca96
Pedro Brügger <65832126+BruggerPedro@users.noreply.github.com>
2022-03-29 14:21:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env pybricks-micropython
+
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, ColorSensor, 
 from pybricks.parameters import Port, Color
 from pybricks.robotics import DriveBase
 from pybricks.tools import StopWatch
+
 # Initialize the EV3 brick.
 ev3 = EV3Brick()
+
 # Initialize.
 test_motorB = Motor(Port.B)
 test_motorC = Motor(Port.C)
 sensorc1 = ColorSensor(Port.S1)
 sensorc2 = ColorSensor(Port.S2)
 cronometro = StopWatch()
+
 #robo = DriveBase(test_motorB,test_motorC,8.5,24)
 
 ev3.speaker.beep()
