894c616590c4c33ee51f4f3a713d2cb3bae3cb35
Zart161z <81576381+Zart161z@users.noreply.github.com>
2022-03-31 19:37:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,63 @@
 #!/usr/bin/env pybricks-micropython
-
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor, ColorSensor, 
-from pybricks.parameters import Port, Color
+from pybricks.ev3devices import Motor, ColorSensor
+from pybricks.parameters import Port, Color, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import StopWatch
-
 # Initialize the EV3 brick.
 ev3 = EV3Brick()
-
 # Initialize.
-test_motorB = Motor(Port.B)
-test_motorC = Motor(Port.C)
-sensorc1 = ColorSensor(Port.S1)
-sensorc2 = ColorSensor(Port.S2)
+motorB = Motor(Port.B)
+motorC = Motor(Port.C)
+motorA = Motor(Port.A)
+#sensorc1 = ColorSensor(Port.S1)
+#sensorc2 = ColorSensor(Port.S2)
+sensorc3 = ColorSensor(Port.S3)
 cronometro = StopWatch()
-
 #robo = DriveBase(test_motorB,test_motorC,8.5,24)
 
-ev3.speaker.beep()
-
-while sensorc1.color()!=Color.BLACK or sensorc2.color()!=Color.BLACK:
-    test_motorB.run(150)
-    test_motorC.run(150)
+def andar_igual(vel,time):
     cronometro.reset()
-    if(sensorc1.color()==Color.BLACK):
-        test_motorB.run(-300)
-        test_motorC.run(150)
-    if(sensorc2.color()==Color.BLACK):
-        test_motorB.run(150)
-        test_motorC.run(-300)
+    while(cronometro.time()<time):
+        motorB.run(vel)
+        vB = motorB.speed()
+        motorC.run(vB)
+    motorB.hold()
+    motorC.hold()
 
-test_motorB.stop()
-test_motorC.stop()
+def andar_bloco():
+    while(sensorc3.reflection()<10):
+        motorB.run(50)
+        motorC.run(50)
+    andar_igual(-50,1500)
+
+def identifica_bloco():
+    mediaV=280 #velocidade media no duty cycle referido
+    n=0
+    media=[]
+    motorA.reset_angle(0)
+    while True:
+        motorA.dc(40)
+        velA=motorA.speed()
+        if(velA>250):
+            media.append(velA) #joga os valores válidos (sem ruído) em uma lista
+            if(len(media)==5): #quando a lista atinge 5 valores calcula a média entre eles
+                for i in range(5): 
+                    mediaV = mediaV + media[i]
+                mediaV = mediaV/6
+                media.clear()
+                n=n+1
+        if(n>10 and mediaV<255): #descarta os 50 primeiros valores válidos (aceleração incial) antes de comparar
+            print(mediaV)
+            break
+    angGarra = motorA.angle()
+    if(angGarra<470):
+        return 1 #bloco grande
+    else:
+        return 0 #bloco pequeno
+
+def main():
+    andar_bloco()
+    identifica_bloco()
+
+main()
